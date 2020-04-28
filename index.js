8bf0f405081176718a5887aa3c04281a49b4d68e
Jonathan Sarmiento <john.sarmiento@me.com>
2020-04-28 17:34:01 -0500

    Added Temp Questions

--- index.js
+++ index.js
@@ -1,50 +1,119 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-    var writeTodo = function(todo){
-        if (todo){
-            $('#doList').append(`
-                <div class="input-group mb-3">
-                    <div class="input-group-prepend">
-                        <div class="input-group-text">
-                            <input type="checkbox" aria-label="Checkbox for following text input">
-                        </div>
-                    </div>
-                    <input type="text" class="form-control" value="${todo}" aria-label="Text input with checkbox">
-                    <div class="input-group-append">
-                        <button class="btn btn-outline-secondary text-white bg-danger delTask" >Delete</button>
-                    </div>
-                </div>
-            `);
-        }
-    }
+    // On Click Start Button Will Create Question //
 
+    $(document).on("click", '.start', function() {
+        $("#question").remove();
+        console.log("Restart Click Works");
 
-// Add button will create a new task //
+        var confirm = "Start Click Works";
+        console.log(confirm);
 
-    $('#todo-add').on('click', function(){
-        var todo = $("#todo").val();
-        console.log(todo);
-        writeTodo(todo);
-        $('#todo').val('');
+        $('#quizBox').append(`
+            <div class="container" id="question">
+                <p>Question 1 Goes here, How do i get data to return from a radio type input after the answer has been selected?</p>
+                <label><input type="radio" name="questions"> answer 1</label><br>
+                <label><input type="radio" name="questions"> answer 1</label><br>
+                <label><input type="radio" name="questions"> answer 1</label><br>
+                <label><input type="radio" name="questions"> answer 1</label><br>
+                <button class="btn btn-primary submit1" type="button">Submit</button>
+            </div>
+        `);
+
+        // This Deletes the Starting Content //
+        $("#start").remove();
+
     });
 
-//Delete button will remove task //
 
-    $(document).on("click", '.delTask', function(){
-        $(this).closest("div.input-group.mb-3").remove();
-        console.log("item");
+    // submit1-5 button will remove Question1-5 and add next Question //
+
+    $(document).on("click", '.submit1', function() {
+        $("#question").remove();
+        console.log("Submit Button 1 Works");
+
+        $('#quizBox').append(`
+            <div class="container" id="question">
+                <p>Question 1 Goes here, How do i get data to return from a radio type input after the answer has been selected?</p>
+                <label><input type="radio" name="questions"> answer 2</label><br>
+                <label><input type="radio" name="questions"> answer 2</label><br>
+                <label><input type="radio" name="questions"> answer 2</label><br>
+                <label><input type="radio" name="questions"> answer 2</label><br>
+                <button class="btn btn-primary submit2" type="button">Submit</button>
+            </div>
+        `);
+
     });
 
-// type press enter auto adds to list //
 
-    $('#todo').keydown(function(){
-        var keycode = (event.keyCode ? event.keyCode : event.which);
-        if (keycode == '13') {
-            var todo = $("#todo").val();
-            console.log(todo);
-            writeTodo(todo);
-            $('#todo').val('');
-        }
+    $(document).on("click", '.submit2', function() {
+        $("#question").remove();
+        console.log("Submit Button 2 Works");
+
+        $('#quizBox').append(`
+            <div class="container" id="question">
+                <p>Question 1 Goes here, How do i get data to return from a radio type input after the answer has been selected?</p>
+                <label><input type="radio" name="questions"> answer 3</label><br>
+                <label><input type="radio" name="questions"> answer 3</label><br>
+                <label><input type="radio" name="questions"> answer 3</label><br>
+                <label><input type="radio" name="questions"> answer 3</label><br>
+                <button class="btn btn-primary submit3" type="button">Submit</button>
+            </div>
+        `);
+
     });
+
+
+    $(document).on("click", '.submit3', function() {
+        $("#question").remove();
+        console.log("Submit Button 3 Works");
+
+        $('#quizBox').append(`
+            <div class="container" id="question">
+                <p>Question 1 Goes here, How do i get data to return from a radio type input after the answer has been selected?</p>
+                <label><input type="radio" name="questions"> answer 4</label><br>
+                <label><input type="radio" name="questions"> answer 4</label><br>
+                <label><input type="radio" name="questions"> answer 4</label><br>
+                <label><input type="radio" name="questions"> answer 4</label><br>
+                <button class="btn btn-primary submit4" type="button">Submit</button>
+            </div>
+        `);
+
+    });
+
+
+    $(document).on("click", '.submit4', function() {
+        $("#question").remove();
+        console.log("Submit Button 4 Works");
+
+        $('#quizBox').append(`
+            <div class="container" id="question">
+                <p>Question 1 Goes here, How do i get data to return from a radio type input after the answer has been selected?</p>
+                <label><input type="radio" name="questions"> answer 5</label><br>
+                <label><input type="radio" name="questions"> answer 5</label><br>
+                <label><input type="radio" name="questions"> answer 5</label><br>
+                <label><input type="radio" name="questions"> answer 5</label><br>
+                <button class="btn btn-primary submit5" type="button">Submit</button>
+            </div>
+        `);
+
+    });
+
+
+    $(document).on("click", '.submit5', function() {
+        $("#question").remove();
+        console.log("Submit Button 5 Works");
+
+        $('#quizBox').append(`
+            <div class="container" id="question">
+                <h2>RESULTS</h2>
+                <
+                <button class="btn btn-primary btn-block start" type="button" id="start">Restart</button>
+            </div>
+        `);
+
+    });
+
+
 
 });
